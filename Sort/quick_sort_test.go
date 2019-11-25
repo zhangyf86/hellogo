c4f5ed84d868c20ba2b848fd2383dc5bdb388a26
zhangyf86 <58064783+zhangyf86@users.noreply.github.com>
2019-11-25 10:53:48 +0800

    重构

---
+++ Sort/quick_sort_test.go
@@ -0,0 +1,23 @@
+package Sort
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	n := make([]int, 20)
+	for i := 0; i < len(n); i++ {
+		n[i] = rand.Intn(20)
+	}
+	QuickSort(IntSlice(n))
+	t.Log(n)
+}
+
+func BenchmarkQuickSort(t *testing.B) {
+	n := make([]int, 20)
+	for i := 0; i < len(n); i++ {
+		n[i] = rand.Intn(20)
+	}
+	QuickSort(IntSlice(n))
+}
