a09a026df8d65545c48943a11e7171b52382f799
zhangyf86 <58064783+zhangyf86@users.noreply.github.com>
2019-11-25 13:35:38 +0800

    重构

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -8,6 +8,12 @@ func separateSort(data InterFace, start, end int) {
 	if start >= end {
 		return
 	}
+	i := partition(data, start, end)
+	separateSort(data, start, i-1)
+	separateSort(data, i+1, end)
+}
+
+func partition(data InterFace, start, end int) int {
 	i := start
 	for j := start; j < end; j++ {
 		if data.Less(end, j) {
@@ -18,6 +24,5 @@ func separateSort(data InterFace, start, end int) {
 		}
 	}
 	data.Swap(end, i)
-	separateSort(data, start, i-1)
-	separateSort(data, i+1, end)
+	return i
 }
